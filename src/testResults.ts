3cbf24fb0fed5e29146c9a9d02f77dc8355c01ca
Cameron Little <cameron@camlittle.com>
2020-11-13 13:09:51 +0100

    Ignore error when using only-changed-files and no tests are found

--- src/testResults.ts
+++ src/testResults.ts
@@ -158,6 +158,11 @@ export class TestResultsManager
     try {
       parsed = JSON.parse(line);
     } catch (err) {
+      if (line.trim() == "No tests found related to files changed since last commit.") {
+        // expected - jest has extra output that doesn't get wrapped in the custom reporter
+        // when using only changed file watcher
+        return;
+      }
       console.warn("Failed to parse jest output");
       console.log(line);
       return;
